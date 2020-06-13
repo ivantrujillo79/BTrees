777b411dcd693d32734b0f89f7790d3717d2fa6e
Iván Trujillo <ivan.trujillo.79@gmail.com>
2020-06-13 14:20:02 +0000

    This is just the initial push of the project

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("This program is just a simple sample about using BTrees as the extraordinary data structure thtey are :)");
+
+    }
+}
